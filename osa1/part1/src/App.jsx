4986d092533e5246cbdfa4d1d90987b1e6d7ac6d
Aleksi Hyppönen <aleksi.hypponen@gmail.com>
2024-06-12 12:27:56 +0300

    osa1a

---
+++ osa1/part1/src/App.jsx
@@ -0,0 +1,15 @@
+const App = () => {
+  const friends = [
+    { name: 'Leevi', age: 4 },
+    { name: 'Venla', age: 10 },
+  ]
+
+  return (
+    <>
+      <p>{friends[0].name} {friends[0].age}</p>
+      <p>{friends[1].name} {friends[1].age}</p>
+    </>
+  )
+}
+
+export default App
